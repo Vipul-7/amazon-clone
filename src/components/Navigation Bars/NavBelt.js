8c398b077a499cf6b92a46e87d3fe1ccf1e0decf
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-06 23:00:17 +0530

    navBelt created

---
+++ src/components/Navigation Bars/NavBelt.js
@@ -0,0 +1,87 @@
+import styles from "./NavBelt.module.scss";
+import Image from "next/image";
+import AmazonLogo from "./AmazonLogo";
+import LocationIcon from "../Icons/LocationIcon";
+import SearchIcon from "../Icons/SearchIcon";
+import DrowDownIcon from "../Icons/DrowDownIcon";
+import CartMain from "../Icons/CartMain";
+
+const NavBelt = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles["nav-left"]}>
+        <div className={styles["nav-left__logo"]}>
+          <AmazonLogo />
+          <h4 className={styles["nav-left__logo-text"]}>.in</h4>
+        </div>
+
+        <div className={styles["nav-left__location"]}>
+          <span>
+            <LocationIcon />
+          </span>
+          <div className={styles["nav-left__location-texts"]}>
+            <p>Hello</p>
+            <h3>Select your address</h3>
+          </div>
+        </div>
+      </div>
+
+      <form className={styles["nav-form"]}>
+        <div className={styles["nav-form__dropdown"]}>
+          <select>
+            <option>All</option>
+          </select>
+        </div>
+        <div className={styles["nav-form__search-input"]}>
+          <input type="text" placeholder="Search Amazon.in" />
+        </div>
+        <div className={styles["nav-form__search-button"]}>
+          <button type="submit">
+            <SearchIcon />
+          </button>
+        </div>
+      </form>
+
+      <div className={styles["nav-right"]}>
+        <section className={styles["nav-right__lang"]}>
+          <span>
+            <Image src="/flag.png" width={20} height={20} alt="flag" />
+          </span>
+          <span>EN</span>
+          <span>
+            <DrowDownIcon />
+          </span>
+        </section>
+
+        <section className={styles["nav-right__signIn-main"]}>
+          <div className={styles["nav-right__signIn"]}>
+            <span className={styles["nav-right__signIn-text1"]}>
+              Hello, sign in
+            </span>
+            <span className={styles["nav-right__signIn-text2"]}>
+              Account & Lists
+            </span>
+          </div>
+          <DrowDownIcon />
+        </section>
+
+        <section className={styles["nav-right__orders"]}>
+          <span className={styles["nav-right__returns"]}>Returns</span>
+          <span className={styles["nav-right__orders-child"]}>& Orders</span>
+        </section>
+
+        <section className={styles["nav-right__cart"]}>
+          <div className={styles["nav-right__cart-detail"]}>
+            <span className={styles["nav-right__cart-detail-number"]}>0</span>
+            <span className={styles["nav-right__cart-detail-icon"]}>
+              <CartMain />
+            </span>
+          </div>
+          <span className={styles["nav-right__cart-text"]}>Cart</span>
+        </section>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBelt;
