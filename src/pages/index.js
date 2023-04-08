50731882372530ac629c2766cc07a78832bae1e6
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-08 15:25:48 +0530

    image sliders added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import NavBelt from "@/components/Navigation Bars/NavBelt";
 import Head from "next/head";
 import NavBarMain from "@/components/Navigation Bars/NavBarMain";
 import SideNavigation from "@/components/Navigation Bars/SideNavigation";
+import ImageSliders from "@/components/Navigation Bars/ImageSliders";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <SideNavigation />
       <NavBelt />
       <NavBarMain />
+      <ImageSliders/>
     </>
   );
 }
