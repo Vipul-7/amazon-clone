ce8397e3c225bf419c2ce3f3122a96593599e788
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-18 00:49:56 +0530

    scroll issue solved

--- src/components/Cart page components/CheckoutDetail.js
+++ src/components/Cart page components/CheckoutDetail.js
@@ -3,20 +3,44 @@ import styles from "./CheckoutDetail.module.scss";
 import CorrectIcon from "../Icons/CorrectIcon";
 import YellowButton from "../Layouts/YellowButton";
 import ChevronDownIcon from "../Icons/ChevronDownIcon";
+import Button from "../Layouts/Button";
 
 const CheckoutDetail = (props) => {
   return (
     <div className={styles.main}>
       <section className={styles["upper-text"]}>
+        {/* <div> */}
+        <CorrectIcon />
         <div>
-          <CorrectIcon />
-          <p>Your order is eligible for FREE delivery</p>
+          <p className={styles.delivery}>
+            Your order is eligible for FREE delivery.
+          </p>
+          <section>
+            <p>Select this option at checkout.</p>
+            <Button text="Details" />
+          </section>
         </div>
-        <p>Select this option at checkout</p>
       </section>
-      <section className={styles.subTotal}>
-        <span>SubTotal ({props.totalItems}):</span>
-        <span>{props.subTotal}</span>
+      <section className={styles["second-division"]}>
+        <div className={styles.subTotal}>
+          <span className={styles["subTotal__text"]}>
+            Subtotal ({props.totalItems} items):
+          </span>
+          <span className={styles["subTotal__amount"]}>
+            <span
+              style={{
+                fontSize: "14px",
+              }}
+            >
+              ₹
+            </span>
+            {props.subTotal}
+          </span>
+        </div>
+        <div className={styles.gift}>
+          <input type="checkbox" />
+          <label>This order contains gift</label>
+        </div>
       </section>
       <section className={styles.button}>
         <YellowButton text="Proceed to checkout" />
--- src/components/Cart page components/index.js
+++ src/components/Cart page components/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import CartItems from './CartItems'
-import styles from './index.module.scss';
-import CheckoutDetail from './CheckoutDetail';
+import React from "react";
+import CartItems from "./CartItems";
+import styles from "./index.module.scss";
+import CheckoutDetail from "./CheckoutDetail";
 
 const CartPageComponents = () => {
   return (
     <div className={styles.main}>
-        <CartItems/>
-        <div>
-            <CheckoutDetail/>
-        </div>
+      <CartItems />
+      <div>
+        <CheckoutDetail subTotal="2,246.00" totalItems="10" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartPageComponents
+export default CartPageComponents;
