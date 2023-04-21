d912b7935b9e070a19e768a0da441ea7e19c1698
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-21 17:39:53 +0530

    product card completed

--- src/components/Specific category products/CategoryProducts.js
+++ src/components/Specific category products/CategoryProducts.js
@@ -1,70 +1,91 @@
-import ProductCard from "./ProductCard"
-import styles from './CategoryProducts.module.scss'
+import ProductCard from "./ProductCard";
+import styles from "./CategoryProducts.module.scss";
 
-const CategoryProducts = () =>{
-    const DUMMY_PRODUCTS = [
-        {
-            id : 'i1',
-            image:"https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
-            title:"Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38\" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...",
-            offerPrice:1899,
-            actualPrice:4999,
-            offerPercentage:62,
-        },
-        {
-            id : 'i2',
-            image:"https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
-            title:"Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38\" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...",
-            offerPrice:1899,
-            actualPrice:4999,
-            offerPercentage:62,
-        },
-        {
-            id : 'i3',
-            image:"https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
-            title:"Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38\" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...",
-            offerPrice:1899,
-            actualPrice:4999,
-            offerPercentage:62,
-        },
-        {
-            id : 'i4',
-            image:"https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
-            title:"Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38\" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...",
-            offerPrice:1899,
-            actualPrice:4999,
-            offerPercentage:62,
-        },
-        {
-            id : 'i5',
-            image:"https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
-            title:"Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38\" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...",
-            offerPrice:1899,
-            actualPrice:4999,
-            offerPercentage:62,
-        }
-    ]
+const CategoryProducts = () => {
+  const DUMMY_PRODUCTS = [
+    {
+      id: "i1",
+      image:
+        "https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
+      title:
+        'Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...',
+      offerPrice: 1899,
+      actualPrice: 4999,
+      offerPercentage: 62,
+    },
+    {
+      id: "i2",
+      image:
+        "https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
+      title:
+        'Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...',
+      offerPrice: 1899,
+      actualPrice: 4999,
+      offerPercentage: 62,
+    },
+    {
+      id: "i3",
+      image:
+        "https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
+      title:
+        'Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...',
+      offerPrice: 1899,
+      actualPrice: 4999,
+      offerPercentage: 62,
+    },
+    {
+      id: "i4",
+      image:
+        "https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
+      title:
+        'Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...',
+      offerPrice: 1899,
+      actualPrice: 4999,
+      offerPercentage: 62,
+    },
+    {
+      id: "i5",
+      image:
+        "https://m.media-amazon.com/images/I/61TapeOXotL._AC_UL600_FMwebp_QL65_.jpg",
+      title:
+        'Noise Newly Launched Twist Bluetooth Calling Smart Watch with 1.38" TFT Biggest Display, Up-to 7 Days Battery, 100+ Watch Faces, IP68, Heart Rate...',
+      offerPrice: 1899,
+      actualPrice: 4999,
+      offerPercentage: 62,
+    },
+  ];
 
-    return (
-        <div className={styles.main}>
-            <section className={styles["extra-info"]}>
+  return (
+    <>
+      <div className={styles.header}>
+        <h3>24 results</h3>
+        <select name="sort" id="sort">
+          <option value="popularity">Popularity</option>
+          <option value="price">Price</option>
+          <option value="discount">Discount</option>
+        </select>
+      </div>
+      <div className={styles.main}>
+        <section className={styles["extra-info"]}></section>
 
-            </section>
-            <section className={styles.products}>
-                {/* <h2>RESULTS</h2> */}
-                {DUMMY_PRODUCTS.map((product) => (
-                    <ProductCard key={product.id}
-                    image={product.image}
-                    title={product.title}
-                    offerPrice={product.offerPrice}
-                    actualPrice={product.actualPrice}
-                    offerPercentage={product.offerPercentage}
-                    />
-                )
-                )}
-            </section>
-        </div>
-    )
-}
+        <section className={styles.products}>
+          <h2>RESULTS</h2>
+          <section className={styles["products-cards"]}>
+            {DUMMY_PRODUCTS.map((product) => (
+              <ProductCard
+                key={product.id}
+                image={product.image}
+                title={product.title}
+                offerPrice={product.offerPrice}
+                actualPrice={product.actualPrice}
+                offerPercentage={product.offerPercentage}
+              />
+            ))}
+          </section>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default CategoryProducts;
--- src/components/Specific category products/ProductCard.js
+++ src/components/Specific category products/ProductCard.js
@@ -1,22 +1,66 @@
-import styles from './ProductCard.module.scss';
+import CheckIcon from "../Icons/CheckIcon";
+import ChevronDownIcon from "../Icons/ChevronDownIcon";
+import CorrectIcon from "../Icons/CorrectIcon";
+import RatingStarEmpty from "../Icons/RatingStarEmpty";
+import RatingStarFull from "../Icons/RatingStarFull";
+import RightArrowIcon from "../Icons/RightArrowIcon";
+import styles from "./ProductCard.module.scss";
 
-const ProductCard = (props) =>{
-    return <div className={styles.main}>
-        <section className={styles.image}>
-            <img src={props.image} alt="product"/>
+const ProductCard = (props) => {
+  return (
+    <div className={styles.main}>
+      <section className={styles.image}>
+        <img src={props.image} alt="product" />
+      </section>
+      <section className={styles.details}>
+        <h4 className={styles.title}>{props.title}</h4>
+        <section className={styles["rating-info"]}>
+          <section className={styles.icons1}>
+            <RatingStarFull />
+            <RatingStarFull />
+            <RatingStarFull />
+            <RatingStarFull />
+            <RatingStarEmpty />
+          </section>
+          <section className={styles.diff}>
+            {" "}
+            <ChevronDownIcon />
+            <span>2,472</span>
+          </section>
         </section>
-        <section className={styles.details}>
-            <h4 className={styles.title}>{props.title}</h4>
-            <section className={styles["rating-info"]}>
-                 
-            </section>
-            <div className={styles["extra-label"]}>Limited time deal</div>
-            <section>
-
-            </section>
-            <section></section>
+        <div className={styles["extra-label"]}>Limited time deal</div>
+        <section className={styles["price-details"]}>
+          <div className={styles["price-details__upper"]}>
+            <span className={styles["price-details__upper-offerPrice"]}>
+              <span>₹</span>
+              {props.offerPrice}
+            </span>
+            <span className={styles.span2}>
+              <span>₹</span>
+              <s>{props.actualPrice}</s>{" "}
+            </span>
+            <span className={styles.span3}>({props.offerPercentage}% off)</span>
+          </div>
+          <div className={styles["price-details__lower"]}>
+            <p>Buy 2 items, get 5% off</p>
+          </div>
         </section>
+        <section className={styles["delivery-details"]}>
+          <div className={styles["delivery-details__upper"]}>
+            <span>
+              <CheckIcon />
+            </span>
+            <span className={styles.prime}>prime </span>
+            <span>Get it by </span>
+            <span className={styles.date}>Monday, 24 April</span>
+          </div>
+          <div className={styles["delivery-details__lower"]}>
+            FREE Delivery by Amazon
+          </div>
+        </section>
+      </section>
     </div>
-}
+  );
+};
 
 export default ProductCard;
